fa5cadc834984015d4e8686d364bd0c42c57d47b
Ralf Klammer <milkbread@freenet.de>
2017-03-24 11:37:03 +0100

    Build logout and round-up login in navbar

--- ka/urls.py
+++ ka/urls.py
@@ -20,8 +20,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     url(r'', include('core.urls')),
     url(r'^login/$', auth_views.login, name='login'),
-    url(r'^(?!/)logout/$', auth_views.logout, {'next_page': '/'},
-        name='logout'),
+    url(r'^(?!/)logout/$', auth_views.logout, name='logout'),
     url(r'^oauth/', include('social_django.urls', namespace='social')),
     url(r'^admin/', admin.site.urls),
 ]
